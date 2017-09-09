a4d39e04caa463cfd575bbe8577a08b27fd3ff62
care4truth <ananth.peddinti@gmail.com>
2017-09-09 17:13:34 -0600

    My project setup completed

---
+++ net.care4truth.shoppingbackend-0.0.1-SNAPSHOT/src/main/java/net/care4truth/shoppingbackend/App.java
@@ -0,0 +1,13 @@
+package net.care4truth.shoppingbackend;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
