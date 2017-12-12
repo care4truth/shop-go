b82830ad155db3a31258e05b010991a7fc14b65f
care4truth <ananth.peddinti@gmail.com>
2017-12-11 17:39:14 -0700

    Adding slf4j logging facility

--- net.care4truth.shopgo-0.0.1-SNAPSHOT/src/main/java/net/care4truth/shopgo/controller/PageController.java
+++ net.care4truth.shopgo-0.0.1-SNAPSHOT/src/main/java/net/care4truth/shopgo/controller/PageController.java
@@ -1,5 +1,7 @@
 package net.care4truth.shopgo.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -14,6 +16,8 @@ import net.care4truth.shoppingbackend.dto.Product;
 @Controller
 public class PageController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(PageController.class);
+	
 	@Autowired
 	private CategoryDAO categoryDAO;
 	
@@ -24,7 +28,8 @@ public class PageController {
 	public ModelAndView index() {
 		ModelAndView mv = new ModelAndView("page");
 		mv.addObject("title", "Home");
-		
+		logger.info("Inside Pagecontroller index method - INFO");
+		logger.debug("Inside Pagecontroller index method - DEBUG");
 		//passing three categories 
 		mv.addObject("categories",categoryDAO.list() );
 		
