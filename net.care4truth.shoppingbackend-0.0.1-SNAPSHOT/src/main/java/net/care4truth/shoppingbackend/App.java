26df7207b4466c53d38976fc0f1bf528aca3382b
care4truth <ananth.peddinti@gmail.com>
2018-03-12 00:59:28 -0600

    Spring boot the existing non-web backend application

--- net.care4truth.shoppingbackend-0.0.1-SNAPSHOT/src/main/java/net/care4truth/shoppingbackend/App.java
+++ net.care4truth.shoppingbackend-0.0.1-SNAPSHOT/src/main/java/net/care4truth/shoppingbackend/App.java
@@ -1,13 +1,27 @@
 package net.care4truth.shoppingbackend;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.ComponentScan;
+
 /**
  * Hello world!
  *
  */
-public class App 
+@SpringBootApplication
+@ComponentScan(basePackages="net.care4truth.shoppingbackend.daoimpl")
+public class App extends SpringBootServletInitializer
 {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application;
+		
+	}
+	
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        SpringApplication.run(App.class,args);
     }
 }
